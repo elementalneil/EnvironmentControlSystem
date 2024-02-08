cf37db4598e55e498dd36529fbce0191140e2225
Shivam-Gole <143583412+Shivam-Gole@users.noreply.github.com>
2024-02-08 12:15:56 +0530

    Added OutsideTempLib

---
+++ EnvironmentControlSystem/OutsideTemp/IOutsideTempSensor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideTemp
+{
+    public interface IOutsideTempSensor
+    {
+        int GetOutsideTemp();
+    }
+}
---
+++ EnvironmentControlSystem/OutsideTemp/OutsideTempSensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideTemp
+{
+    public class OutsideTempSensor : IOutsideTempSensor
+    {
+        public int GetOutsideTemp()
+        {
+            // Simulated method to get outside temperature
+            Random rand = new Random();
+            return rand.Next(0, 50); // Random temperature between 0 to 50 Celsius
+        }
+    }
+}
