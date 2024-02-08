4226b7e5af5be709d55c931deef1b078471ca1e3
Madhu <68181437+Madhumitha7765@users.noreply.github.com>
2024-02-08 14:00:15 +0530

    Added interfaceLibs
     PeopleCounter, TemperaturRegulator

---
+++ EnvironmentControlSystem/TemperatureRegulatorLib/TempRegulator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ITemperatureRegulatorLib;
+
+namespace TemperatureRegulatorLib
+{
+    public class TempRegulator : ITempRegulator
+    {
+        public void RegulateTemperature(int newTemperature)
+        {
+            Console.WriteLine($"Regulating temperature to {newTemperature} Celsius");
+            // Simulated method to regulate temperature
+        }
+    }
+}
+
