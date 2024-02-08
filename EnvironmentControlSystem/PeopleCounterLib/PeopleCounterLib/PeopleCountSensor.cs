433aed8c92c4dde0f1a9fe31d2744abbd36a08f1
Madhu <68181437+Madhumitha7765@users.noreply.github.com>
2024-02-08 12:18:26 +0530

    added temperatureregulator lib, peoplecounter lib

---
+++ EnvironmentControlSystem/PeopleCounterLib/PeopleCounterLib/PeopleCountSensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleCounterLib
+{
+    public class PeopleCountSensor : IPeopleCountSensor
+    {
+        public int GetPeopleCount()
+        {
+            // Simulated method to get people count
+            Random rand = new Random();
+            return rand.Next(1, 6); // Random number of people between 1 to 5
+        }
+    }
+}
---
+++ EnvironmentControlSystem/TemperatureRegulatorLib/TempRegulator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureRegulatorLib
+{
+    public class TempRegulator : ITempRegulator
+    {
+        public void RegulateTemperature(int newTemperature)
+        {
+            Console.WriteLine($"Regulating temperature to {newTemperature} Celsius");
+            // Simulated method to regulate temperature
+        }
+    }
+}
+
