e79dd193045c083fa6d54954932126a7e9da68b2
Sankhanil Nayek <nayek.sankhanil@gmail.com>
2024-02-08 12:26:28 +0530

    Added Environment Control Unit Library

---
+++ EnvironmentControlSystem/EnvironmentControlUnitLib/ECU.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace EnvironmentControlUnit {
+    public class ECU {
+        private readonly IOutsideTempSensor outsideTempSensor;
+        private readonly IPeopleCountSensor peopleCountSensor;
+        private readonly ITempCalculator tempCalculator;
+        private readonly ITempRegulator tempRegulator;
+
+        public ECU(IOutsideTempSensor outsideTempSensor, IPeopleCountSensor peopleCountSensor,
+                    ITempCalculator tempCalculator, ITempRegulator tempRegulator) {
+            this.outsideTempSensor = outsideTempSensor;
+            this.peopleCountSensor = peopleCountSensor;
+            this.tempCalculator = tempCalculator;
+            this.tempRegulator = tempRegulator;
+        }
+
+        public void RunTemperatureControl() {
+            while (true) {
+                int outsideTemp = outsideTempSensor.GetOutsideTemp();
+                int peopleCount = peopleCountSensor.GetPeopleCount();
+
+                int newTemperature = tempCalculator.CalculateNewTemperature(peopleCount, outsideTemp);
+                tempRegulator.RegulateTemperature(newTemperature);
+
+                Thread.Sleep(1000); // Sleep for 30 seconds
+            }
+        }
+    }
+}
